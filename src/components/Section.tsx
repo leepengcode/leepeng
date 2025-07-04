259c3b001bc0b9f82e4bd4bd09089a0fe2a88af4
Lui Peng <norngliipeng98.info@gmail.com>
2025-07-04 21:27:39 +0700

    Add to index

--- src/components/Section.tsx
+++
@@ -1,9 +0,0 @@
-const Section = ({ id, children, className = "" }) => {
-    return (
-        <section id={id} className={`py-24 ${className}`} data-aos="fade-up">
-            {children}
-        </section>
-    );
-};
-
-export default Section;
